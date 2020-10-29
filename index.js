1530ecd6d1c072486b28fc7f21da8138b77f7e29
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-10-29 19:46:55 +0300

    add popup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('hello world');
+
+const btnDiploma = document.querySelector('.button_diplom');
+const popup = document.querySelector('.popup');
+
+
+btnDiploma.addEventListener('click', function(e) {
+    popup.classList.remove('hide');
+})
