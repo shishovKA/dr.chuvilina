506185086e3fc9ad59f8863993d861795bff442a
Konstantin Shishov <shishov.k.a@yandex.ru>
2021-02-08 11:04:12 +0300

    Updates

--- dr.chuvilina.e31bb0bc.js
+++ dr.chuvilina.e31bb0bc.js
@@ -186,7 +186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6393" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12643" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
